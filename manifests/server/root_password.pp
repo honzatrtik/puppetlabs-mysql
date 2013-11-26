e9dda5a1810d4b617fcb8cb77ed114e8e250d6f4
honzatrtik <honza.trtik@gmail.com>
2013-11-26 13:32:45 +0100

    root@* #3

--- manifests/server/root_password.pp
+++ manifests/server/root_password.pp
@@ -5,14 +5,14 @@ class mysql::server::root_password {
 
   # manage root password if it is set
   if $mysql::server::root_password != 'UNSET' {
-    mysql_user { 'root@*':
+    mysql_user { 'root@localhost':
       ensure        => present,
       password_hash => mysql_password($mysql::server::root_password),
     }
 
     file { "${::root_home}/.my.cnf":
       content => template('mysql/my.cnf.pass.erb'),
-      require => Mysql_user['root@*'],
+      require => Mysql_user['root@localhost'],
     }
   }
 
