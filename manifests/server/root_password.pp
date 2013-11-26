7faad1016a05eef9217d56512eb674481f0aa9e7
honzatrtik <honza.trtik@gmail.com>
2013-11-26 13:22:52 +0100

    root@*

--- manifests/server/root_password.pp
+++ manifests/server/root_password.pp
@@ -5,7 +5,7 @@ class mysql::server::root_password {
 
   # manage root password if it is set
   if $mysql::server::root_password != 'UNSET' {
-    mysql_user { 'root@localhost':
+    mysql_user { 'root@*':
       ensure        => present,
       password_hash => mysql_password($mysql::server::root_password),
     }
